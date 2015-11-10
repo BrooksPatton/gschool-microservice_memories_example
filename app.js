5db6fae1824f3c0d56e839f469a4974c0c2f5d10
bpatton <bpatton@brooks-MacBook-Pro.local>
2015-11-10 10:18:57 -0700

    swagger ui installed and configured

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+
 var server = app.listen(PORT, function () {
 	console.log('Server running on port %d', server.address().port);
 });
