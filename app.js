1ac1ee4f75a5275d0a9e4d524140fce7f7655e82
bpatton <bpatton@brooks-MacBook-Pro.local>
2015-11-10 09:56:57 -0700

    basic express app created

---
+++ app.js
@@ -0,0 +1,9 @@
+var PORT = process.env.PORT || 3000;
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(PORT, function () {
+	console.log('Server running on port %d', server.address().port);
+});
